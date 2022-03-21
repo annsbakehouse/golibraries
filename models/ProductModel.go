66167251a37ab46f7544c66ac58f83ad446dd503
bananasmall <84952666+ykzirla@users.noreply.github.com>
2022-03-21 10:57:09 +0700

    Product Model

---
+++ models/ProductModel.go
@@ -0,0 +1,81 @@
+package models
+
+import (
+	"gorm.io/gorm"
+	"time"
+	"gorm.io/plugin/soft_delete"
+)
+
+type ProductModel struct {
+	ID           	string     `gorm:"column:id;primary_key" json:"id"`
+	Name         	NullString     `gorm:"column:name;default:null" json:"name"`
+	PID          	NullString     `gorm:"column:pid;default:null" json:"pid"`
+	ProductTypeId	NullString		`gorm:"column:product_type_id" json:"product_type_id"`
+	Active	 	int		`gorm:"column:active" json:"active"`
+	CreatedBy    NullString  	`gorm:"column:created_by" json:"created_by"`
+	CreatedAt    time.Time 		`gorm:"column:created_at" json:"crated_at"`
+	UpdatedBy    NullString 	`gorm:"column:updated_by" json:"updated_by"`
+	UpdatedAt    time.Time `gorm:"column:updated_at" sql:"type:timestamp without time zone" json:"updated_at"`
+	DeletedBy	 NullString `gorm:"column:updated_by" json:"deleted_by"`
+	DeletedAt soft_delete.DeletedAt `gorm:"uniqueIndex:udx_name;column:deleted_at" json:"deleted_at"`
+	ProductType  ProductTypeModel `gorm:"foreignKey:ID;references:ProductTypeId" json:"product_type"`
+}
+
+type ProductModelPreload struct {
+	ID           	string     `gorm:"column:id;primary_key" json:"id"`
+	Name         	NullString     `gorm:"column:name;default:null" json:"name"`
+	PID          	NullString     `gorm:"column:pid;default:null" json:"pid"`
+	ProductTypeId	NullString		`gorm:"column:product_type_id" json:"product_type_id"`
+	Active	 	int		`gorm:"column:active" json:"active"`
+	ProductType  ProductTypeModel `gorm:"foreignKey:ID;references:ProductTypeId" json:"product_type"`
+}
+
+
+
+// TableName sets the insert table name for this struct type
+func (p *ProductModel) TableName() string {
+	return "product"
+}
+func (p *ProductModelPreload) TableName() string {
+	return "product"
+}
+
+func (p *ProductModel) BeforeCreate(tx *gorm.DB) (err error) {
+	return
+}
+
+func (p *ProductModel) BeforeUpdate(tx *gorm.DB) (err error) {
+	return
+}
+func (p *ProductModel) AfterUpdate(tx *gorm.DB) (err error) {
+	return
+}
+func (p *ProductModel) BeforeDelete(tx *gorm.DB) (err error) {
+	// fmt.Println("Before Delete")
+	_,con,_ := DbConnect()
+	var model InventoryMaterialPackagingModel
+	con.Model(&model).Where("id=?", p.ID).Update("deleted_by",ActiveUser)
+	return
+}
+
+//strcture input
+type ProductModelInput struct {
+	Name         	NullString     `gorm:"column:name;default:null" json:"name" binding:"required"`
+	PID          	NullString     `gorm:"column:pid;default:null" json:"pid" binding:"required"`
+	ProductTypeId	NullString		`gorm:"column:product_type_id" json:"product_type_id" binding:"required"`
+	Active	 	int		`gorm:"column:active" json:"active"`
+	ProductTypeId 	NullString		`gorm:"column:product_type_id" json:"product_type_id" binding:"required"`
+}
+type ProductModelUpdate struct {
+	ID	string	`json:"id" binding:"required"`
+	Name         	NullString     `gorm:"column:name;default:null" json:"name" binding:"required"`
+	PID          	NullString     `gorm:"column:pid;default:null" json:"pid" binding:"required"`
+	ProductTypeId	NullString		`gorm:"column:product_type_id" json:"product_type_id" binding:"required"`
+	Active	 	int		`gorm:"column:active" json:"active"`
+	ProductTypeId 	NullString		`gorm:"column:product_type_id" json:"product_type_id" binding:"required"`
+}
+type ProductModelInfo struct {
+	ID	string	`json:"id" binding:"required"`
+}
+
+
--- models/ProductTypeModel.go
+++ models/ProductTypeModel.go
@@ -1,47 +1,16 @@
 package models
 
 import (
-	//"database/sql"
-
 	"time"
-
 	"gorm.io/gorm"
 )
 
 type ProductTypeModel struct {
 	ID        string     `gorm:"column:id;primary_key" json:"id"`     //
 	Name      NullString `gorm:"column:name" json:"name"`             //
-	CreatedBy NullString `gorm:"column:created_by" json:"created_by"` //
-	CreatedAt NullString `gorm:"column:created_at" json:"created_at"`
-	UpdatedBy NullString `gorm:"column:updated_by" json:"updated_by"` //
-	UpdatedAt NullString `gorm:"column:updated_at" json:"updated_at"`
-	// DeletedBy NullString `gorm:"column:deleted_by" json:"deleted_by"` //
-	// DeletedAt NullString `gorm:"column:deleted_at" json:"deleted_at"`
 }
 
 // TableName sets the insert table name for this struct type
 func (p *ProductTypeModel) TableName() string {
 	return "product_type"
 }
-
-func (p *ProductTypeModel) BeforeCreate(tx *gorm.DB) (err error) {
-	tt := time.Now()
-	tFormat := tt.Format("2006-01-02 15:04:05")
-	p.CreatedAt = NullStringInput(tFormat)
-	// p.UpdatedAt = NullStringInput(tFormat)
-	return
-}
-
-func (p *ProductTypeModel) BeforeUpdate(tx *gorm.DB) (err error) {
-	tt := time.Now()
-	tFormat := tt.Format("2006-01-02 15:04:05")
-	p.UpdatedAt = NullStringInput(tFormat)
-	return
-}
-
-// func (b *ProductTypeModel) BeforeDelete(tx *gorm.DB) (err error) {
-// 	tt := time.Now()
-// 	tFormat := tt.Format("2006-01-02 15:04:05")
-// 	d.DeletedAt = NullStringInput(tFormat)
-// 	return
-// }
