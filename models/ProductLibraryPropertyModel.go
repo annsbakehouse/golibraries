98cbf1f26592deaaa61b60f58c9a71b3af272bb7
bananasmall <84952666+ykzirla@users.noreply.github.com>
2022-03-22 17:53:35 +0700

    model

--- models/ProductLibraryPropertyModel.go
+++ models/ProductLibraryPropertyModel.go
@@ -6,10 +6,11 @@ import (
 	"gorm.io/plugin/soft_delete"
 )
 
-type ProductLibraryTitleModel struct {
+type ProductLibraryPropertyModel struct {
 	ID           	string     `gorm:"column:id;primary_key" json:"id"`
 	Name         	NullString     `gorm:"column:name;default:null" json:"name"`
 	ProductLibraryCategoryId string `gorm:"column:product_library_category_id" json:"product_library_category_id"`
+	ProductLibraryTitleId string `gorm:"column:product_library_title_id" json:"product_library_title_id"`
 	CreatedBy    NullString  	`gorm:"column:created_by" json:"created_by"`
 	CreatedAt    time.Time 		`gorm:"column:created_at" json:"crated_at"`
 	UpdatedBy    NullString 	`gorm:"column:updated_by" json:"updated_by"`
@@ -18,52 +19,56 @@ type ProductLibraryTitleModel struct {
 	DeletedAt soft_delete.DeletedAt `gorm:"uniqueIndex:udx_name;column:deleted_at" json:"deleted_at"`
 }
 
-type ProductLibraryTitleModelPreload struct {
+type ProductLibraryPropertyModelPreload struct {
 	ID           	string     `gorm:"column:id;primary_key" json:"id"`
 	Name         	NullString     `gorm:"column:name;default:null" json:"name"`
 	ProductLibraryCategoryId string `gorm:"column:product_library_category_id" json:"product_library_category_id"`
+	ProductLibraryTitleId string `gorm:"column:product_library_title_id" json:"product_library_title_id"`
 	ProductLibraryCategory ProductLibraryCategoryModelPreload `gorm:"foreignKey:ID;references:ProductLibraryCategoryId" json:"product_library_category_data"`
+	ProductLibraryTitle ProductLibraryTitleModelPreload `gorm:"foreignKey:ID;references:ProductLibraryTitleId" json:"product_library_title_data"`
 	DeletedAt soft_delete.DeletedAt `gorm:"column:deleted_at" json:"-"`
 }
 
 
 
 // TableName sets the insert table name for this struct type
-func (p *ProductLibraryTitleModel) TableName() string {
-	return "product_library_title"
+func (p *ProductLibraryPropertyModel) TableName() string {
+	return "product_library_property"
 }
-func (p *ProductLibraryTitleModelPreload) TableName() string {
-	return "product_library_title"
-}
-
-func (p *ProductLibraryTitleModel) BeforeCreate(tx *gorm.DB) (err error) {
-	return
+func (p *ProductLibraryPropertyModelPreload) TableName() string {
+	return "product_library_property"
 }
 
-func (p *ProductLibraryTitleModel) BeforeUpdate(tx *gorm.DB) (err error) {
+func (p *ProductLibraryPropertyModel) BeforeCreate(tx *gorm.DB) (err error) {
 	return
 }
-func (p *ProductLibraryTitleModel) AfterUpdate(tx *gorm.DB) (err error) {
+
+func (p *ProductLibraryPropertyModel) BeforeUpdate(tx *gorm.DB) (err error) {
 	return
 }
-func (p *ProductLibraryTitleModel) BeforeDelete(tx *gorm.DB) (err error) {
+func (p *ProductLibraryPropertyModel) AfterUpdate(tx *gorm.DB) (err error) {
+	return
+}
+func (p *ProductLibraryPropertyModel) BeforeDelete(tx *gorm.DB) (err error) {
 	// fmt.Println("Before Delete")
 	_,con,_ := DbConnect()
-	var model ProductLibraryTitleModel
+	var model ProductLibraryPropertyModel
 	con.Model(&model).Where("id=?", p.ID).Update("deleted_by",ActiveUser)
 	return
 }
 
 //strcture input
-type ProductLibraryTitleModelInput struct {
+type ProductLibraryPropertyModelInput struct {
 	Name         	string     	`json:"name" binding:"required"`
 	ProductLibraryCategoryId string `json:"product_library_category_id" binding:"required"`
+	ProductLibraryTitleId string `json:"product_library_title_id" binding:"required"`
 }
-type ProductLibraryTitleModelUpdate struct {
+type ProductLibraryPropertyModelUpdate struct {
 	ID	string	`json:"id" binding:"required"`
 	Name         	string     	`json:"name" binding:"required"`
 	ProductLibraryCategoryId string `json:"product_library_category_id" binding:"required"`
+	ProductLibraryTitleId string `json:"product_library_title_id" binding:"required"`
 }
-type ProductLibraryTitleModelInfo struct {
+type ProductLibraryPropertyModelInfo struct {
 	ID	string	`json:"id" binding:"required"`
 }
